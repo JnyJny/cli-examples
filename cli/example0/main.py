c346b59a64828a7134f96b755b0da7f11cbe0f03
Erik OShaughnessy <erik.oshaughnessy@gmail.com>
2021-07-01 16:50:25 +0000

    Added .replit file for use with replit.com

--- cli/example0/main.py
+++ cli/example0/main.py
@@ -1,12 +1,18 @@
-"""
+"""CLI using sys.argv
+
 """
 
 import sys
 
-def main() -> None:
 
+# called by the stub created by poetry from pyproject.toml
+def main() -> None:
+    """
+    """
     print("example0", __file__)
     print(f"argv={sys.argv}")
 
+
+# only needed if the file is executed directly
 if __name__ == '__main__':
     main()
